0f8158014d6e13f791ebe95dafd50c9c29cb3657
Fillsogood <jdd05273@naver.com>
2024-01-25 21:13:40 +0900

    코드 리팩토링

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ db = pymysql.connect(
 	cursorclass=pymysql.cursors.DictCursor
 )
 cur = db.cursor()
-def execute_query(conn,query,args=None):
-    with conn.cursor() as cursor:
-        cursor.execute(query,args or ())
-        if query.strip().upper().startswith('SELECT'):
-            return cursor.fetchall()
-        else:   
-            conn.commit()   
+
+# 페이지네이션 total 갯수 구하는 execute 함수
+def first_execute(args):
+	cur.execute(args)
+	sum = cur.fetchone()
+	sum = int(sum['sum'])
+	return sum
 
 @app.route('/',methods=("GET",))
 def index():
@@ -28,344 +28,70 @@ def index():
 	category = request.args.get('category')
 	# 검색 name 가져오기
 	search_q = request.args.get('q')
-	sql = "SELECT count(*) as sum FROM kream"
-	cur.execute(sql)
-	sum = cur.fetchone()
-	sum = int(sum['sum'])
 
-	sql = "SELECT * FROM kream LIMIT %s OFFSET %s"
-	# sql = "SELECT * FROM kream LIMIT %s OFFSET %s"
-	cur.execute(sql,(per_page,offset))
-	kream_data = cur.fetchall()
-	kream_data_len = len(kream_data)
-	pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True) 
-	
-    # 카테고리 선택 기능
-	if category=="상의":
-		# 검색 기능 카테고리 : 상의 에서 제품 검색
+	# 전체 카테고리 빼고 모드 카테고리 검색
+	if category and category!="전체":
+		#카테고리 선택후 제품명 검색 코드
 		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
+			sql1 = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
+			sum = first_execute(sql1)
 
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
+			sql2 = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}' LIMIT {per_page} OFFSET {offset}"
+			cur.execute(sql2)
 			kream_data = cur.fetchall()
 			kream_data_len = len(kream_data)
 			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		#카테고리 상의 검색
+		#카테고리 검색 코드
 		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
+			sql1 = f"SELECT count(*) as sum FROM kream WHERE category='{category}'"
+			sum = first_execute(sql1)
 
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
+			sql2 = f"SELECT * FROM kream WHERE category='{category}' LIMIT {per_page} OFFSET {offset}"
+			cur.execute(sql2)
 			kream_data = cur.fetchall()
 			kream_data_len = len(kream_data)
 			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-			
-	elif category=="하의":
-		
-		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-	elif category=="신발":
-		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-			
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-	elif category=="패션잡화":
-		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-			
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-	elif category=="아우터":
-		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-			
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-	elif category=="가방":
-		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-			
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-	elif category=="지갑":
-		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-			
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-	elif category=="시계":
-		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-			
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-	elif category=="컬렉터블":
-		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-			
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-	elif category=="뷰티":
-		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-			
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-	elif category=="테그":
-		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-			
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-	elif category=="캠핑":
-		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-			
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-	elif category=="가구/리빙":
-		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-			
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
-		else:
-			sql = "SELECT count(*) as sum FROM kream WHERE category=%s"
-			cur.execute(sql,(category))
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
-
-			sql = "SELECT * FROM kream WHERE category=%s LIMIT %s OFFSET %s"
-			cur.execute(sql,(category,per_page,offset))
-			kream_data = cur.fetchall()
-			kream_data_len = len(kream_data)
-			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
+	#전체 카테고리 따로 검색
 	elif category=="전체":
 		if search_q:
-			sql = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%' and category = '{category}'"
-			cur.execute(sql)
-			sum = cur.fetchone()
-			sum = int(sum['sum'])
+			sql1 = f"SELECT count(*) as sum FROM kream WHERE product_name LIKE '%{search_q}%'"
+			sum = first_execute(sql1)
 
-			sql = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%'"
-			cur.execute(sql)
+			sql2 = f"SELECT * FROM kream WHERE product_name LIKE '%{search_q}%' LIMIT {per_page} OFFSET {offset}"
+			cur.execute(sql2)
 			kream_data = cur.fetchall()
 			kream_data_len = len(kream_data)
 			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
+			#카테고리 상의 검색
+		else:
+			print("a")
+			sql1 = "SELECT count(*) as sum FROM kream"
+			sum = first_execute(sql1)
 
+			sql2 = f"SELECT * FROM kream LIMIT {per_page} OFFSET{offset}"
+			cur.execute(sql2)
+			kream_data = cur.fetchall()
+			kream_data_len = len(kream_data)
+			pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
+	#처음 페이지 시작
+	else:
+		sql1 = "SELECT count(*) as sum FROM kream"
+		sum = first_execute(sql1)
+
+		sql2 = f"SELECT * FROM kream LIMIT {per_page} OFFSET {offset}"
+		# sql = "SELECT * FROM kream LIMIT %s OFFSET %s"
+		cur.execute(sql2)
+		kream_data = cur.fetchall()
+		kream_data_len = len(kream_data)
+		pagination=Pagination(page=page,total=sum,per_page=per_page,prev_label="Previous",next_label="Next",format_total=True)
+		 
 	return render_template('indext.html',kream_data = kream_data,kream_data_len=kream_data_len,pagination=pagination,search=True,bs_version=5)
-
 
 
 @app.route('/delete',methods=("POST",))
 def delete():
 	ids = request.form['ids']
 	ids = ids.split(",")[0:-1]
-	print(ids)
 	for id in ids:
 		sql = f'DELETE FROM kream WHERE ID={id}'
 		cur.execute(sql)
